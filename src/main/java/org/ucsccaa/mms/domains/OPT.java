3e2e06c2ae312dca92b498a1266e9cf0958c3873
Eirc <dongzihao123@126.com>
2020-11-12 18:41:00 -0800

    CAA-83, fix, fix ID strategy

--- src/main/java/org/ucsccaa/mms/domains/OPT.java
+++ src/main/java/org/ucsccaa/mms/domains/OPT.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class OPT {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String status;
 
